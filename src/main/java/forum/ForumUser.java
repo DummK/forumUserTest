4c6236008871a8f884c7af732bd29a4a2637368a
Filip <fpolski22@gmail.com>
2025-04-02 13:49:41 +0200

    Dokonczyc

--- src/main/java/forum/ForumUser.java
+++ src/main/java/forum/ForumUser.java
@@ -3,5 +3,9 @@ package forum;
 import java.util.List;
 
 public class ForumUser {
+    private String username;
+    private String realName;
+    private List<String> posts;
+    private List<String> comments;
 
 }
--- src/test/java/forum/tdd/ForumUserTestSuite.java
+++ src/test/java/forum/tdd/ForumUserTestSuite.java
@@ -1,8 +1,16 @@
 package forum.tdd;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class ForumUserTestSuite {
+    @BeforeEach
+    public void beforeEach() {
+
+    }
+
+
+
     @Test
     public void testAddForumPost() {
 
