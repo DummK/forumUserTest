ab0e2005bf466d8ff066cfc51efe4bd144a36d5e
Filip <fpolski22@gmail.com>
2025-04-03 13:54:55 +0200

    Koniec

--- src/main/java/forum/ForumUser.java
+++ src/main/java/forum/ForumUser.java
@@ -1,11 +1,45 @@
 package forum;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class ForumUser {
     private String username;
     private String realName;
-    private List<String> posts;
-    private List<String> comments;
+    private List<String> posts = new ArrayList<String>();
+    private List<String> comments = new ArrayList<String>();
 
+    public ForumUser(String username, String realName) {
+        this.username = username;
+        this.realName = realName;
+    }
+
+    public void addPost(String post) {
+        posts.add(post);
+    }
+
+    public void addComment(String comment) {
+        comments.add(comment);
+    }
+
+    public void deletePost(String post) {
+        posts.remove(post);
+    }
+
+    public void deleteComment(String comment) {
+        comments.remove(comment);
+    }
+
+    public String getUsername() {
+        return username;
+    }
+    public String getRealName() {
+        return realName;
+    }
+    public List<String> getPosts() {
+        return posts;
+    }
+    public List<String> getComments() {
+        return comments;
+    }
 }
--- src/test/java/forum/tdd/ForumUserTestSuite.java
+++ src/test/java/forum/tdd/ForumUserTestSuite.java
@@ -1,33 +1,76 @@
 package forum.tdd;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import forum.ForumUser;
+import org.junit.jupiter.api.*;
 
 public class ForumUserTestSuite {
+    private ForumUser forumUser;
+
     @BeforeEach
-    public void beforeEach() {
+        public void beforeEach() {
+            System.out.println("Test started");
+        }
 
-    }
+    @AfterEach
+        public void afterEach() {
+            System.out.println("Test finished");
+        }
 
+    @DisplayName("Testing if posts gets added to forum")
+        @Test
+            public void testAddForumPost() {
+                //Given
+                forumUser = new ForumUser("John Doe", "John Doe");
+                String post = "Hello World!";
 
+                //When
+                forumUser.addPost(post);
 
-    @Test
-    public void testAddForumPost() {
+                //Then
+                Assertions.assertTrue(forumUser.getPosts().contains(post));
+        }
 
-    }
+    @DisplayName("Testing if comments gets added to forum")
+        @Test
+            public void testAddForumComment() {
+                //Given
+                forumUser = new ForumUser("John Doe", "John Doe");
+                String comment = "Hello World!";
 
-    @Test
-    public void testAddForumComment() {
+                //When
+                forumUser.addComment(comment);
 
-    }
+                //Then
+                Assertions.assertTrue(forumUser.getComments().contains(comment));
+        }
 
-    @Test
-    public void testDeleteForumPost() {
+    @DisplayName("Testing if posts gets deleted from forum")
+        @Test
+            public void testDeleteForumPost() {
+                //Given
+                forumUser = new ForumUser("John Doe", "John Doe");
+                String post = "Hello World!";
+                forumUser.addPost(post);
 
-    }
+                //When
+                forumUser.deletePost(post);
 
-    @Test
-    public void testDeleteForumComment() {
+                //Then
+                Assertions.assertFalse(forumUser.getPosts().contains(post));
+        }
 
-    }
+    @DisplayName("Testing if comments gets deleted from forum")
+        @Test
+            public void testDeleteForumComment() {
+                //Given
+                forumUser = new ForumUser("John Doe", "John Doe");
+                String comment = "Hello World!";
+                forumUser.addComment(comment);
+
+                //When
+                forumUser.deleteComment(comment);
+
+                //Then
+                Assertions.assertFalse(forumUser.getComments().contains(comment));
+        }
 }
